ed0fbb59e8381cf3842f7391f0598f5bc5de91da
Nathan Colson <n.colson@students.ephec.be>
2024-12-04 21:18:56 +0100

    début test unitaire

---
+++ app/nathan_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from app import Flashcard
+
+### Tests unitaires de Nathan ###
+
+class TestsNathan(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
